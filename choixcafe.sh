d13a6eb0805f06b18e891214bd6af6c420cb830f
M Benslim <mbe5610@gmail.com>
2025-03-20 14:38:03 +0100

    Ajout initial du choixcafe.sh

---
+++ choixcafe.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Choisissez votre boisson :"
+echo "1) Café"
+echo "2) Thé"
+echo "3) Chocolat chaud"
+echo "4) Eau"
+
+read -p "Votre choix : " choix
+
+case $choix in
+  1) echo "Voici votre café." ;;
+  2) echo "Voici votre thé." ;;
+  3) echo "Voici votre chocolat." ;;
+  4) echo "Voici votre eau." ;;
+  *) echo "Choix invalide." ;;
+esac
